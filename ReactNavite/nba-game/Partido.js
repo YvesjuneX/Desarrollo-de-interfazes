1558d20f0ae075178566fd15fe973d3cbe736afa
Juan Leal <juanleal132005@gmail.com>
2026-01-19 00:22:45 +0100

    casi terminado

--- ReactNavite/nba-game/Partido.js
+++ ReactNavite/nba-game/Partido.js
@@ -1,13 +1,76 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
-export default function Partido({ navigation }) {
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+export default function Partido({ route, navigation }) {
+
+  const { equipoIz, equipoDe } = route.params || {};
   return (
-    <View>
-      <Text>Estamos en el seleccion</Text>
-      <Button 
-        title="Ir a seleccion"
-        // El nombre debe coincidir EXACTAMENTE con el name="" del Stack.Screen
-        onPress={() => navigation.navigate('Seleccion')}
-      />
+    <View style={styles.contenedorPrincipal}>
+
+      <View style={styles.equiposContenedor}>
+
+        {/* Equipo Izquierdo */}
+        <View style={styles.columna}>
+          <Image
+            source={{ uri: equipoIz.logo_url }}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+          <Text style={styles.nombreEquipo}>{equipoIz.name}</Text>
+
+          <View style={styles.listaJugadores}>
+            {equipoIz.top_players.map((jugador, index) => (
+              <Text key={index}>• {jugador}</Text>
+            ))}
+          </View>
+        </View>
+
+        {/* Equipo Derecho */}
+        <View style={styles.columna}>
+          <Image
+            source={{ uri: equipoDe.logo_url }}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+          <Text style={styles.nombreEquipo}>{equipoDe.name}</Text>
+
+          <View style={styles.listaJugadores}>
+            {equipoDe.top_players.map((jugador, index) => (
+              <Text key={index}>• {jugador}</Text>
+            ))}
+          </View>
+        </View>
+
+      </View>
+
     </View>
   );
 }
+const styles = StyleSheet.create({
+  contenedorPrincipal: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 50,
+  },
+  equiposContenedor: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  columna: {
+    width: '45%',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 110,
+    marginBottom: 10,
+  },
+  nombreEquipo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  listaJugadores: {
+    marginTop: 15,
+  },
+});
--- ReactNavite/nba-game/Seleccion.js
+++ ReactNavite/nba-game/Seleccion.js
@@ -1,50 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Equipos from './Equipos.json';
 import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
 export default function Seleccion({ navigation }) {
+  
+  const [numEquipoIzq, setnumEquipoIzq] = useState(0);
+  const [numEquipoDer, setnumEquipoDer] = useState(1);
+  const equipoIzq = Equipos[numEquipoIzq];
+  const equipoDer = Equipos[numEquipoDer];
+  function cambiarEquipo(equipo) {
+    if(equipo==0){
+      if (numEquipoIzq==5){
+        setnumEquipoIzq(0)
+      }else{
+        setnumEquipoIzq(numEquipoIzq+1)
+      }
+    }else {
+      if (numEquipoDer==5){
+        setnumEquipoDer(0)
+      }else{
+        setnumEquipoDer(numEquipoDer+1)
+      }
+    }
+   
+
+  };
   return (
-    <View style={styles.container}>
-      
-      {/* Contenedor de los dos equipos */}
-      <View style={styles.rivalesContainer}>
-        
-        {/* Equipo 1 (Izquierda) */}
+     <View style={styles.contenedorPrincipal}>
+
+      <View style={styles.equiposContenedor}>
+
+        {/* Equipo Izquierdo */}
         <View style={styles.columna}>
-          <Image 
-            source={{ uri: 'https://via.placeholder.com/100' }} 
-            style={styles.logo} 
+          <Image
+            source={{ uri:  equipoIzq.logo_url }}
+            style={styles.logo}
+            resizeMode="contain"
           />
-          <Text style={styles.nombreEquipo}>Bulls</Text>
-          <Button title="Cambiar" onPress={() => {}} />
+          <Text style={styles.nombreEquipo}>{equipoIzq.name}</Text>
+          <Button title="Cambiar" onPress={() => cambiarEquipo(0)} />
+
           <View style={styles.listaJugadores}>
-            <Text>• Jordan</Text>
-            <Text>• Pippen</Text>
-            <Text>• Rodman</Text>
+            {equipoIzq.top_players.map((jugador,index) => (
+              <Text key={index}>• {jugador}</Text>
+            ))}
           </View>
         </View>
 
-        {/* Equipo 2 (Derecha) */}
+        {/* Equipo Derecho */}
         <View style={styles.columna}>
-          <Image 
-            source={{ uri: 'https://via.placeholder.com/100' }} 
-            style={styles.logo} 
+          <Image
+            source={{ uri: equipoDer.logo_url }}
+            style={styles.logo}
+            resizeMode="contain"
           />
-          <Text style={styles.nombreEquipo}>Lakers</Text>
-          <Button title="Cambiar" onPress={() => {}} />
+          <Text style={styles.nombreEquipo}>{equipoDer.name}</Text>
+          <Button title="Cambiar" onPress={() => cambiarEquipo(1)} />
           <View style={styles.listaJugadores}>
-            <Text>• Magic J.</Text>
-            <Text>• Kareem</Text>
-            <Text>• Worthy</Text>
+            {equipoDer.top_players.map((jugador,index) => (
+              <Text key={index}>• {jugador}</Text>
+            ))}
           </View>
         </View>
 
       </View>
 
-      {/* Botón Central para navegar */}
-      <View style={styles.botonCentral}>
-        <Button 
-          title="IR A PARTIDO" 
-          onPress={() => navigation.navigate('Partido')} 
+      <View style={styles.partido}>
+        <Button
+          title="IR A PARTIDO"
+          onPress={() => navigation.navigate('Partido',{
+            equipoIz: equipoIzq,
+            equipoDe: equipoDer,
+          })}
         />
       </View>
 
@@ -53,12 +80,12 @@ export default function Seleccion({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contenedorPrincipal: {
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 50,
   },
-  rivalesContainer: {
+  equiposContenedor: {
     flexDirection: 'row', // Esto pone uno al lado del otro
     justifyContent: 'space-around',
     width: '100%',
@@ -68,20 +95,19 @@ const styles = StyleSheet.create({
     alignItems: 'center', // Centra los elementos dentro de la columna
   },
   logo: {
-    width: 80,
-    height: 80,
-    backgroundColor: '#eee',
+    width: 150,
+    height: 110,
     marginBottom: 10,
   },
   nombreEquipo: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
   },
   listaJugadores: {
     marginTop: 15,
   },
-  botonCentral: {
+  partido: {
     marginTop: 50,
     paddingHorizontal: 20,
   },
